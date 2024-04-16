3bd42f59912b626a4d9c4d57c7f8ae8744d11707
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2024-04-16 15:40:23 +0300

    added new files

--- home/views.py
+++ home/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hey World")
+    #return HttpResponse("Troubleshooting the page ")
+    return render(request, 'home/welcome.html', {})
 
+    #return render(request,'home/welcome.html', {})
