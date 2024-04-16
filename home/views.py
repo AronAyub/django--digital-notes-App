c63051d6b2cc6693763873f36dddcd2ac0482448
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2024-04-16 21:15:40 +0300

    added date module

--- home/views.py
+++ home/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from datetime import datetime
 
 def home(request):
     #return HttpResponse("Troubleshooting the page ")
-    return render(request, 'home/welcome.html', {})
+    return render(request, 'home/welcome.html', {'today': datetime.today()})
 
     #return render(request,'home/welcome.html', {})
