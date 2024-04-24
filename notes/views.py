9e1c2aac5ebe99de640535a1fc6d8988e9b14cae
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2024-04-24 16:05:43 +0300

    user

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,12 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.http.response import HttpResponseRedirect
+from django.forms import BaseModelForm
 from django.shortcuts import render
-from django.http import Http404
+from django.http import Http404, HttpResponse
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import  LoginRequiredMixin
 
 from .forms import NoteForm
 from .models import Notes
@@ -11,24 +16,35 @@ class NotesDeleteView(DeleteView):
     success_url = '/smart/notes'
     template_name = 'notes/notes_delete.html'
    
-
-
-
-
-
 class NotesUpdateView(UpdateView):
     model = Notes
     success_url  = '/smart/notes'
     form_class = NoteForm
     
-class NoteCreateView(CreateView):
+class NoteCreateView(LoginRequiredMixin, CreateView):
     model = Notes
     success_url  = '/smart/notes'
     form_class = NoteForm
-class NotesListView(ListView):
+    login_url = "/admin"
+
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user =  self.request.user
+        self.object.save()
+        return HttpResponse(self.get_success_url)
+    
+
+
+
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes 
     context_object_name = "notes"
     template_name ="notes/notes.list.html"
+    login_url = "/admin"
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 class NotesDetailView(DetailView):
     model = Notes
