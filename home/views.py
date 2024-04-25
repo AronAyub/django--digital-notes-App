9911739cbf1170e265c49df4919a193eec54f35f
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2024-04-25 12:57:37 +0300

    added login

--- home/views.py
+++ home/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserChangeForm
 
+class SignupView(CreateView):
+    form_class = UserChangeForm
+    template_name = 'home/register.html'
+    
 
 class LogoutInterfaceView(LogoutView):
     template_name = 'home/logout.html'
