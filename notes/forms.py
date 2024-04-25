bdfaf5b577b4fbb06c43cde97cda341d6c6d9117
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2024-04-25 22:41:36 +0300

    works fine

--- notes/forms.py
+++ notes/forms.py
@@ -1,4 +1,5 @@
-from  django import forms
+from django import forms
+from django.core.exceptions import ValidationError
 
 from .models import Notes
 
@@ -6,16 +7,15 @@ class NoteForm(forms.ModelForm):
     class Meta:
         model = Notes
         fields = ('title', 'text')
-        widgets  = {
-            'title':forms.TextInput(attrs={'class': 'form-control my-5'}),
-            'text': forms.Textarea(attrs={"class": 'form-countrol my-5'})
-        }   
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control my-5'}),
+            'text': forms.Textarea(attrs={"class": "form-control mb-5"})
+        }
         labels = {
             'text': 'Write your thoughts here:'
         }
-
-    def clean_title(self):
-        title = self.cleaned_data['title']
-        if 'Django' not in title:
-            raise forms.ValidationError('We only accept note about Django !')
-        return title 
+    # def clean_title(self):
+    #     title = self.cleaned_data['title']
+    #     if 'Django' not in title:
+    #         raise forms.ValidationError('We only accept note about Django !')
+    #     return title 
--- notes/views.py
+++ notes/views.py
@@ -1,58 +1,114 @@
-from typing import Any
-from django.db.models.query import QuerySet
 from django.http.response import HttpResponseRedirect
-from django.forms import BaseModelForm
-from django.shortcuts import render
-from django.http import Http404, HttpResponse
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
-from django.contrib.auth.mixins import  LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NoteForm
 from .models import Notes
 
-class NotesDeleteView(DeleteView):
+
+class NotesDeleteView(LoginRequiredMixin, DeleteView):
     model = Notes
     success_url = '/smart/notes'
     template_name = 'notes/notes_delete.html'
-   
-class NotesUpdateView(UpdateView):
+    login_url = "/login"
+
+class NotesUpdateView(LoginRequiredMixin, UpdateView):
     model = Notes
-    success_url  = '/smart/notes'
+    success_url = '/smart/notes'
     form_class = NoteForm
-    
+    login_url = "/login"
+
 class NoteCreateView(LoginRequiredMixin, CreateView):
     model = Notes
-    success_url  = '/smart/notes'
+    success_url = '/smart/notes'
     form_class = NoteForm
-    login_url = "/admin"
-
+    login_url = "/login"
 
     def form_valid(self, form):
         self.object = form.save(commit=False)
-        self.object.user =  self.request.user
+        self.object.user = self.request.user
         self.object.save()
-        return HttpResponse(self.get_success_url)
-    
-
-
+        return HttpResponseRedirect(self.get_success_url())
+        
 class NotesListView(LoginRequiredMixin, ListView):
-    model = Notes 
+    model = Notes
     context_object_name = "notes"
-    template_name ="notes/notes.list.html"
-    login_url = "/admin"
+    template_name = "notes/notes_list.html"
+    login_url = "/login"
 
     def get_queryset(self):
         return self.request.user.notes.all()
 
-class NotesDetailView(DetailView):
+class NotesDetailView(LoginRequiredMixin, DetailView):
     model = Notes
-    context_object_name =  "note"
+    context_object_name = "note"
+    login_url = "/login"
+
+
+
+
+
+
+
+
+
+
+# from typing import Any
+# from django.db.models.query import QuerySet
+# from django.http.response import HttpResponseRedirect
+# from django.forms import BaseModelForm
+# from django.shortcuts import render
+# from django.http import Http404, HttpResponse
+# from django.views.generic import CreateView, DetailView, ListView, UpdateView
+# from django.views.generic.edit import DeleteView
+# from django.contrib.auth.mixins import  LoginRequiredMixin
+
+# from .forms import NoteForm
+# from .models import Notes
+
+# class NotesDeleteView(DeleteView):
+#     model = Notes
+#     success_url = '/smart/notes'
+#     template_name = 'notes/notes_delete.html'
+   
+# class NotesUpdateView(UpdateView):
+#     model = Notes
+#     success_url  = '/smart/notes'
+#     form_class = NoteForm
     
-#receive  a private key
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("Note doesn't exist")
-    return render(request, 'notes/notes_detail.html', {'note': note})
+# class NoteCreateView(LoginRequiredMixin, CreateView):
+#     model = Notes
+#     success_url  = '/smart/notes'
+#     form_class = NoteForm
+#     login_url = "/admin"
+
+
+#     def form_valid(self, form):
+#         self.object = form.save(commit=False)
+#         self.object.user =  self.request.user
+#         self.object.save()
+#         return HttpResponse(self.get_success_url)
+    
+
+
+# class NotesListView(LoginRequiredMixin, ListView):
+#     model = Notes 
+#     context_object_name = "notes"
+#     template_name ="notes/notes.list.html"
+#     login_url = "/admin"
+
+#     def get_queryset(self):
+#         return self.request.user.notes.all()
+
+# class NotesDetailView(DetailView):
+#     model = Notes
+#     context_object_name =  "note"
+    
+# #receive  a private key
+# def detail(request, pk):
+#     try:
+#         note = Notes.objects.get(pk=pk)
+#     except Notes.DoesNotExist:
+#         raise Http404("Note doesn't exist")
+#     return render(request, 'notes/notes_detail.html', {'note': note})
